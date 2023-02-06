fbbd7055759d201bbd54250c194084c3abe90e21
Mercen <mercen@mercen.net>
2023-02-07 07:54:35 +0900

    Initial Commit

---
+++ DGSWchat/DGSWchatApp.swift
@@ -0,0 +1,17 @@
+//
+//  DGSWchatApp.swift
+//  DGSWchat
+//
+//  Created by Mercen on 2023/02/07.
+//
+
+import SwiftUI
+
+@main
+struct DGSWchatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
